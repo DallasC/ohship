52e2e8d45001742410de488e4c371ff807bbc062
Dallas Coble <dallascoble@outlook.com>
2019-06-30 11:27:14 +0800

    set-up
    Basic setup for a quicksilver game.

---
+++ chapters/TOC.md
@@ -0,0 +1,2 @@
+1. set-up
+    Install everything we need and get the project set up and ready to go
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate quicksilver;
+
+use quicksilver::{
+    Result,
+    geom::{Vector},
+    graphics::{Color},
+    lifecycle::{Settings, State, Window, run},
+};
+
+struct Game;
+
+impl State for Game {
+    fn new() -> Result<Game> {
+        Ok(Game)
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::BLACK)?;
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run::<Game>("Oh! Ship", Vector::new(600, 600), Settings::default());
 }
